8a720b6e6b96ae0906f0d06bcd48864fc832890b
leon.j.ward <leon.j.ward@0eddf203-64dc-01f7-0d81-672302c3988f>
2010-05-15 14:37:02 +0000

    Creating 0.1a preview release

--- trunk/install-openfpc.sh
+++ trunk/install-openfpc.sh
@@ -20,7 +20,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #########################################################################################
-openfpcver="0.1"
+openfpcver="0.1a"
 TARGET_DIR="/opt/openfpc"
 INSTALL_FILES="ofpc-extract.pl openfpc openfpc.conf ofpcParse.pm"
 PERL_MODULES="ofpcParse.pm"
