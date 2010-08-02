7cb001c22824fd6b24d387cc3b366c079db93468
Leon Ward <leonward@users.noreply.github.com>
2010-08-02 10:55:50 +0000

    Updated install

--- tools/mk_release.sh
+++ tools/mk_release.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Create a release file for OpenFPC
 # It's simple, dirty, and works for me.
 
 # Leon Ward - leon@rm-rf.co.uk
 
-TARPATH=..
-FILES="ofpc-extract.pl install-openfpc.sh openfpc openfpc.conf README ofpcParse.pm"
-VERFILES="install-openfpc.sh ofpc-extract.pl openfpc"
+TARPATH=~
+SRCPATH=..
+FILES="ofpc-client.pl install-ofpc.sh openfpc openfpc.conf docs/README ofpc/Parse.pm ofpc/Request.pm ofpc-queued.pl"
+VERFILES="install-ofpc.sh ofpc-client.pl openfpc ofpc-queued.pl"
 
 echo Checking version numbers in code...
 for i in $VERFILES
 do
-	VER=$(grep openfpcver $i |awk -F = '{print $2}')
+	VER=$(grep openfpcver $SRCPATH/$i |awk -F = '{print $2}')
 	echo -e " $VER - $i"
 done	
 
-VER=$(grep openfpcver openfpc |awk -F = '{print $2}')
+VER=$(grep openfpcver $SRCPATH/openfpc |awk -F = '{print $2}')
 TARGET="$TARPATH/openfpc-$VER"
 FILENAME="openfpc-$VER.tgz"
 echo -e "* Build Version $VER in $TARPATH ? (ENTER = yes)"
@@ -25,7 +26,10 @@ read
 
 if [ -d $TARGET ]
 then
-	echo Error $TARGET exists
+	echo Error $TARGET exists. 
+	echo Hit ENTER to rm -rf $TARGET, to stop it CRTL+C
+	read 
+	rm -rf $TARGET
 	exit 1
 else
 	echo Creating $TARGET
@@ -34,7 +38,7 @@ else
 	for i in $FILES
 	do
 		echo -e "- Adding $i to $TARGET"
-		cp $i $TARGET
+		cp $SRCPATH/$i $TARGET
 	done
 		cd $TARPATH
 		tar -czf $FILENAME openfpc-$VER
