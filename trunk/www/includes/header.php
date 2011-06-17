d2778efa7eee17aeba16cc3f89b8f74666f68cbc
leon.j.ward <leon.j.ward@0eddf203-64dc-01f7-0d81-672302c3988f>
2011-06-17 13:01:54 +0000

    Added fix for session data into old table. Oops, thought this was already in. Added missing GUI_DB_HOST to config

--- trunk/www/includes/header.php
+++ trunk/www/includes/header.php
@@ -21,7 +21,7 @@
 
 require "includes/config.inc.php";
 
-$head .= "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
+$head = "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
 $head .= "<head>\n";
 $head .= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
 $head .= "<title>OpenFPC Web Interface</title>\n";
