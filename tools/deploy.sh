88e488445dc4bcf49b533ae684a5b1268a131259
Leon Ward <leon@leonward.com>
2014-08-12 20:05:10 +0100

    Added quick deploy script to push from dev to test device(s)

---
+++ tools/deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Deply script to push key files on to testing device. 
+# Only really useful for my own testing - Leon
+
+TUSER=lward
+TDEV=192.168.42.10
+TPATH=/home/$USER/openfpc/
+PATHCHECK="openfpc-client README.md"
+DIRS="cgi-bin docs etc OFPC tools www"
+FILES="openfpc-client openfpc-dbmaint openfpc-password openfpc  openfpc-cx2db  openfpc-install.sh openfpc-queued"
+
+VERBOSE=1
+
+function die
+{
+	echo $1
+	exit 1
+}
+
+if [ $VERBOSE == 1 ] ; then
+	echo "* Running in verbose mode"
+else
+	echo "* Running in silent mode"
+fi
+
+# Check we are in the expected location on the disk
+# E.g run ./tools/deploy
+
+for i in $PATHCHECK;
+do
+	[ $VERBOSE == 1 ] && echo "  Checking for file $i"
+	[ -e "$i" ] || die "! Cant find $i this script should be run from the openfpc top dir."
+done
+
+
+# Check that the target exists on the remote system 
+if $(ssh $TUSER@$TDEV [ -d $TPATH ])
+	then
+		echo "- Target path $TPATH found on target device $TDEV"
+	else
+		die "Cant find target path $TPATH on target device $TDEV, won't continue"
+fi
+
+for d in $DIRS;
+do
+	echo "- Copying $d"
+	scp -r $d $TUSER@$TDEV:$TPATH/
+done
+
+for f in $FILES;
+do
+	scp $f $TUSER@$TDEV:$TPATH/
+done
+
